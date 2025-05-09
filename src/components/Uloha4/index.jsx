36820b7210f84acc9c8ee3dcdbe47bea719c2bd6
Anna Kršková <krskova.aaa@gmail.com>
2025-05-09 23:38:11 +0200

    Vypracovani dalsich casti - Uloha3 a Uloha4

--- src/components/Uloha4/index.jsx
+++ src/components/Uloha4/index.jsx
@@ -6,5 +6,14 @@ Zadání 2: Pokud uživatel z textového políčka vše smaže, vypište „Uži
 */
 
 export const Uloha4 = () => {
-  return <input />;
+  const handleChange = (event) => {
+    const text = event.target.value;
+    if (text === '') {
+      console.log('Uživatel nenapsal nic');
+    } else {
+      console.log('Uživatel napsal: ' + text);
+    }
+  };
+
+  return <input onChange={handleChange} />;
 };
